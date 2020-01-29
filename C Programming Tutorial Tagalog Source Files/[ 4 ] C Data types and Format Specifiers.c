8a377ea2a035a17b10069bd5397523a7586c18e2
Jervx <48374007+Jervx@users.noreply.github.com>
2020-01-29 16:34:12 +0800

    Add files via upload

---
+++ C Programming Tutorial Tagalog Source Files/[ 4 ] C Data types and Format Specifiers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    
+    int num1 = 10;
+    float num2 = 10.5;
+    double num3 = 10.5;
+    char character1 = 'A';
+    char name[] = "BOBCAT";
+    
+    printf("Num1 has %d  -  Num2 has %f  -  num3 has %f  -  Character1 has %c  - Name has value of %s",num1,num2,num3,character1,name);
+}
