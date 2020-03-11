a931e0d3046ce7196852d412383359d917d77cff
Jervx <48374007+Jervx@users.noreply.github.com>
2020-03-12 01:32:40 +0800

    Create tur5.c

---
+++ C Programming Tutorial Tagalog Source Files/tur5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    //example 1
+    int var1;
+    
+    printf("Enter A Number: ");
+    scanf("%d",&var1);
+    
+    printf("The Number You Entered is %d",var1);
+    
+    
+    //example 2
+        
+    char name[10];
+    
+    printf("\nEnter Your Name: ");
+    scanf("%c",&name);
+
+    printf("Your name is %s",name);
+}
