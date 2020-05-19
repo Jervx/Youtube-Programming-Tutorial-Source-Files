bfeb56c0a2bb7e85c16b48d2b6431088d603bd4d
Jervx <louellagracechua@gmail.com>
2020-05-20 00:18:50 +0800

    added by Jervx

---
+++ C Programming Tutorial Tagalog Source Files/[ 6 ] Arithmetic Operators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int x = 5;
+    int y = 1;
+
+    printf("Sum is %d",x+y);
+    printf("Diff is %d",x-y);
+    printf("Prod is %d", x*y);
+    print("Quotient is %d", x/y);
+
+}
